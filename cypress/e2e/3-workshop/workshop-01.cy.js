f8970b2ccd2d2b639b22668c2d0bfacf03bcce47
Coolibin <alexander.kalinich@gmail.com>
2025-08-05 16:37:32 -0700

    Adding workshop-01.cy.js

--- cypress/e2e/3-workshop/workshop-01.cy.js
+++ cypress/e2e/3-workshop/workshop-01.cy.js
@@ -1,14 +1,23 @@
-
 context('Actions', () => {
 
     it('defining a custom command', () => {
         Cypress.Commands.add('getById', (selector, ...args) => {
-            return cy.get(`[id=${selector}]`, ...args)
+            return cy.get(`#${selector}`, ...args)
         })
 
-        cy.visit("https://example.cypress.io/commands/actions")
+        cy.origin('https://example.cypress.io', () => {
+            cy.visit("https://example.cypress.io/commands/actions")
+            cy.contains('a', '.focus()').click()
+        })
 
-        cy.getById("focus").click()
+        cy.origin('https://docs.cypress.io', () => {
+            cy.url().should('include', '/focus');
+            cy.get('h1')
+                .should('exist')  // Verify element exists
+                .and('be.visible') // Verify it's visible
+                .and('have.text', 'focus'); // Verify exact text
+
+        })
     })
 
 })
